11a8c9c271c7253ebaf1f971a4a97ae2a4f2f1ed
GerardoALH <galh88@gmail.com>
2020-09-27 22:10:44 -0600

    Segundo scrip

---
+++ Plot 2.R
@@ -0,0 +1,25 @@
+# Conjunto de datos
+Data <-  read.table(file.path(pathdata, "household_power_consumption.txt"), header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+SubSetData <- Data[Data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+head(Data)
+
+# Creación de gráfico No. 2 -----------------------------------------------
+
+# Formato de hora y fecha
+Date_Time <- strptime(paste(SubSetData$Date, SubSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+GlobalActivePower <- as.numeric(SubSetData$Global_active_power)
+
+# Creación del gráfico
+
+png("plot2.png", width=480, height=480)
+plot(Date_Time, GlobalActivePower, 
+     type= 'l', # Se selecciona el tipo de lineas, en ?plot aparecen varios tipos
+     xlab="", # Se deja sin espacio
+     ylab="Global Active Power (kilowatts)") #Se traza la etiqueta del eje "y"
+dev.off() # Se concluye la acción de guardado
+
+
+
